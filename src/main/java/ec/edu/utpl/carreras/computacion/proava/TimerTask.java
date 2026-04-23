2aee3cf1c96f10a1c5492a635507743156b2c4ac
jhordycamacas <jhordycamacaas9@gmail.com>
2026-04-23 08:25:26 -0500

    Extencion Thread

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
@@ -1,14 +1,16 @@
 package ec.edu.utpl.carreras.computacion.proava;
-
-public class TimerTask {
+//Cambio 1 - La clase extiende Thread
+public class TimerTask extends Thread {
     private final String name;
     private int seconds;
 
     public TimerTask(String name, int seconds) {
+        super(name); // Se agrega super(name);
         this.name = name;
         this.seconds = seconds;
     }
 
+    @Override // Se agrega Override
     public void run() {
         while(seconds > 0) {
             System.out.printf("%s: %d segundos restantes%n", name, seconds);
