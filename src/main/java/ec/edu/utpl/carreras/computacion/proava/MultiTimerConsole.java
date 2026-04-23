8868679398602724c59c0a670a632ed71bc80149
jhordycamacas <jhordycamacaas9@gmail.com>
2026-04-23 08:50:49 -0500

    Solucion Lambda

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -6,7 +6,12 @@ package ec.edu.utpl.carreras.computacion.proava;
 public class MultiTimerConsole {
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+        //Se usa Lambda: () -> expresion implementa Runnable en linea
+
+        new Thread(() -> timerA.run(), "Hilo-A").start();
+        new Thread(() -> timerB.run(), "Hilo-B").start();
+        new Thread(() -> timerC.run(), "Hilo-C").start();
     }
 }
